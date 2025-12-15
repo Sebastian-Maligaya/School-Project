ea70640ba02e027124b31116c6b72ec972f00971
Sebastian Maligaya <maligaya.286449@globalcity.sti.edu.ph>
2025-12-15 21:37:39 +0800

    Update 1
    a

--- src/Homepage/Homepage.jsx
+++ src/Homepage/Homepage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "../components/Navbar.jsx";
+import Navbar from "../Navbar/Navbar.jsx";
 import "./Homepage.css";
 
 export default function Homepage() {
