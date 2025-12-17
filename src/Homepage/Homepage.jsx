18d7d086b765b61575fcb2121a005c7252336ed9
Sebastian Maligaya <maligaya.286449@globalcity.sti.edu.ph>
2025-12-18 00:51:56 +0800

    update 3
    new design

--- src/Homepage/Homepage.jsx
+++ src/Homepage/Homepage.jsx
@@ -3,7 +3,7 @@ import "./Homepage.css";
 
 export default function Homepage() {
   return (
-    <div>
+    <div className="hp-page">
 
       <header id="home" className="hp-hero">
         <h1>Senator Renato "Compañero" Cayetano Memorial Science and Technology High School</h1>
@@ -16,14 +16,37 @@ export default function Homepage() {
       </header>
 
       <main className="hp-main">
-        <section id="about" className="hp-section">
-          <h2>About Us</h2>
-          <p>
-            We provide a supportive learning environment focused on curiosity and
-            student growth. Use this template as a starting point for your school's
-            homepage.
-          </p>
-        </section>
+      <section id="about" className="hp-section">
+          <div className="hp-about-grid">
+            <div className="hp-left">
+              <h2>About our School</h2>
+
+              <p>The Sunrays stands for the light that illuminates your path in building a temple of immortal souls in the future generations; the Open Book means that anyone may come to engraft a branch of knowledge into the stock of wisdom. The Mountains and Sunrays represents hope of a new beginning.</p>
+
+              <p>The Gear symbolizes technology and know-how in the modern society. The Building represents the City of Taguig of modern infrastructures whilst the White Duck represents Pateros. The Laurel leaves represent the dignity of man and serve as the emblem of victory, of a courageous flight of success.</p>
+
+              <p>This is the meaning of our seal, revere it, cherish it, and lives by it always.</p>
+
+              <table>
+                <tr>
+                  <td className="number">1200+</td>
+                  <td className="number">100+</td>
+                  <td className="number">95%</td>
+                </tr>
+                <tr>
+                  <td className="label">Students</td>
+                  <td className="label">Teachers</td>
+                  <td className="label">Success Rate</td>
+                </tr>
+              </table>
+            </div>
+
+            <div className="hp-right">
+              <img className = "about-img" src="https://cdn.britannica.com/19/148619-004-113E7353.jpg" alt="School placeholder" />
+            </div>
+          </div>
+
+      </section>
 
         <section id="Achievement" className="hp-section">
           <h2>Achievement</h2>
