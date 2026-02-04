fe5502d06f84aab51585d83280be776be74c64ee
maligayasteven-bit <maligayasteven@gmail.com>
2026-02-05 01:48:26 +0800

    FACILITIES BUTTONS

---
+++ src/Facilities/Facilities Subpages/canteen.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import "../Homepage/Homepage.css"
+import StrandCard from '../Homepage/Card';
+import "./Facilities.css";
+import Facilities_Ground_Floor from "../assets/Facilities_Ground_Floor.png";
+import { Link } from 'react-router-dom';
